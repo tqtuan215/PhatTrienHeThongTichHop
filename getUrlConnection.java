e6a2434a67880c2a51a4dc0fa8922dbc688c9d32
tqtuan215 <72064300+tqtuan215@users.noreply.github.com>
2020-10-09 16:39:50 +0700

    Add files via upload

---
+++ getUrlConnection.java
@@ -0,0 +1,19 @@
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+public class getUrlConnection {
+ public static void main(String[] args) {
+ URL u;
+ URLConnection uc;
+ try {
+ u = new URL("http://iuh.edu.vn");
+ uc=u.openConnection();
+ } catch (MalformedURLException e) {
+	 e.printStackTrace();
+	 } catch (IOException e) {
+	 e.printStackTrace();
+	 }
+	 }
+	}
