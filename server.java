f2e8ffe2a01a79de6fed2249dd519452741c14aa
tqtuan215 <72064300+tqtuan215@users.noreply.github.com>
2020-10-19 21:41:38 +0700

    update

--- server.java
+++ server.java
@@ -1,34 +1,37 @@
 package lab3;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 public class server {
 public static void main(String[] args) {
 
 try {
-	ServerSocket server = new ServerSocket(9540);
+	ServerSocket ss = new ServerSocket(9541);
 	System.out.println("Server da duoc tao");
-	Socket client = server.accept();
+	Socket client = ss.accept();
 	System.out.println("Client da ket noi den server");
 	Scanner inFromClient = new Scanner(client.getInputStream());
 	PrintStream outToClient = new PrintStream(client.getOutputStream());
 	outToClient.println("Xin chao, ban muon tim sach gi?");
 	String tens = inFromClient.nextLine();
 	System.out.println("client: " + tens);
-	BufferedReader br= new BufferedReader(new FileReader("D:\\text.txt"));
-	String tep=br.readLine();
-	if(tep.contains(tens))
-		outToClient.println("Co sach " + tens);
-	else
-		outToClient.println("Rat tiec khong co sach " + tens);
-	} catch (IOException e) {
-// TODO Auto-generated catch block
+	String fileName = "D:\\sach.txt";//bạn hãy thay đổi đường dẫn tới file của bạn
+	String S = new String(Files.readAllBytes(Paths.get(fileName)),StandardCharsets.UTF_8);
+		if(S.contains(tens))
+			outToClient.println("Co sach " + tens);
+		else
+			outToClient.println("Rat tiec khong co sach " + tens);
+		
+     } 
+	
+	
+	 catch (IOException e) {
 		e.printStackTrace();
 }
 }
